25b97198da3d2b2212db15ad02237e46a7def21e
sajidibnimohd <msajidibni@gmail.com>
2023-03-11 22:15:38 +0530

    commit

---
+++ binary.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int binarysearch(int arr[], int size, int key)
+{
+    int start = 0;
+    int end = size - 1;
+    int mid = (start + end) / 2;
+    while (start <= end)
+    {
+        
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+        if (key > arr[mid])
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+        mid = (start + end) / 2;
+    }
+    return -1;
+}
+
+int main()
+{
+    int even[] = {2, 4, 6, 8, 12, 18};
+    int evenindex = binarysearch(even, 6, 12);
+    cout << "index of 12 is " << evenindex << endl;
+    return 0;
+}
